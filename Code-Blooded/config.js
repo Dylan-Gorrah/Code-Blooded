4219f6ee5eb4688201f2f2e3c282af9f50adc6bb
Dylan LLuigi Di-Caprio Gorrah <63928272+Dylan-Gorrah@users.noreply.github.com>
2025-10-05 17:23:22 +0200

    Update config.js

--- Code-Blooded/config.js
+++ Code-Blooded/config.js
@@ -1,7 +1,7 @@
 // config.js - Add this to .gitignore
 const CONFIG = {
-    SUPABASE_URL: 'https://bkbeobpoipfgfqcezoya.supabase.co',
-    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrYmVvYnBvaXBmZ2ZxY2V6b3lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwNzY2MDIsImV4cCI6MjA3NDY1MjYwMn0._f9jAZtSOkSeKyF9i-Wv0Qn1OLosWqKJwKzaLPd6V3g'
+    SUPABASE_URL: 'GetYourOwn',
+    SUPABASE_ANON_KEY:GetYourOwn'
 };
 
 // Export for use in other files
@@ -10,3 +10,4 @@ if (typeof module !== 'undefined' && module.exports) {
 } else {
     window.CONFIG = CONFIG;
 }
+
